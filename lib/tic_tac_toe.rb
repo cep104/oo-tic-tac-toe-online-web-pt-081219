a2ebb2bd696498f8dd6cdce6d0469f301cc10305
Candice Peters <candicelizabethpeters@gmail.com>
2019-09-23 21:06:06 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -4,6 +4,18 @@ class TicTacToe
     @board = Array.new(9, " ")
   end
 
+WIN_COMBINATIONS = [
+
+    [0,1,2], # Top row
+    [3,4,5],  # Middle row
+    [6,7,8],  # Bottom row
+    [0,3,6],  # Left col
+    [1,4,7],  # Middle col
+    [2,5,8],  # Right col
+    [0,4,8],  # Diagnol 1
+    [2,4,6]  # Diagnol 2
+  ]
+  
   def display_board
     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
     puts "-----------"
@@ -57,17 +69,7 @@ class TicTacToe
     end
   end
   
-  WIN_COMBINATIONS = [
-
-    [0,1,2], # Top row
-    [3,4,5],  # Middle row
-    [6,7,8],  # Bottom row
-    [0,3,6],  # Left col
-    [1,4,7],  # Middle col
-    [2,5,8],  # Right col
-    [0,4,8],  # Diagnol 1
-    [2,4,6]  # Diagnol 2
-  ]
+  
 
 
 
